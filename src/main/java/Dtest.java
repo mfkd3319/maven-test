551f10eee2c6ad46565878b02128d17eb6821c8d
mfkd <mfkd_3319@163.com>
2017-11-17 15:20:07 +0800

    0001

--- src/main/java/Dtest.java
+++ src/main/java/Dtest.java
@@ -5,7 +5,7 @@ public class Dtest {
 
     public static void main(String args[]){
 
-        System.out.print("333333333");
+        System.out.print("branch-->master");
         System.out.print("18888833333");
         System.out.print("1");
         System.out.print("18833333");
