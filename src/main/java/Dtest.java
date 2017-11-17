5e450cce6db685041ad5a8532398c7e8a769dff1
mfkd <mfkd_3319@163.com>
2017-11-17 16:02:21 +0800

    00019

--- src/main/java/Dtest.java
+++ src/main/java/Dtest.java
@@ -5,7 +5,7 @@ public class Dtest {
 
     public static void main(String args[]){
 
-        System.out.print("18888888313131333333333333333333333333333333333333333");
-        System.out.print("18888888313131333333333333333333333333333333333333333");
+        System.out.print("master");
+
     }
 }
