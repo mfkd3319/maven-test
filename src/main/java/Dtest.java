382e86666448285d08e348ca0c9f45ebf2b2dbd8
mfkd <mfkd_3319@163.com>
2017-11-17 15:15:29 +0800

    0001

--- src/main/java/Dtest.java
+++ src/main/java/Dtest.java
@@ -5,10 +5,10 @@ public class Dtest {
 
     public static void main(String args[]){
 
-        System.out.print("18888888313131333333333333333333333333333333333333333");
-        System.out.print("18888888313131333333333333333333333333333333333333333");
+        System.out.print("333333333");
+        System.out.print("18888833333");
         System.out.print("1");
-        System.out.print("188888883131313333333333333333333333333332333333333333");
+        System.out.print("18833333");
         System.out.print("13");
     }
 }
